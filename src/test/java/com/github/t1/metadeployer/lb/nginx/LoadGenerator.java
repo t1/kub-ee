6861f0de2c8f38f06a3263b3e97ff1f0228fa603
Rüdiger zu Dohna <snackbox@sinntr.eu>
2017-07-20 07:25:49 +0200

    add LoadGenerator

---
+++ src/test/java/com/github/t1/metadeployer/lb/nginx/LoadGenerator.java
@@ -0,0 +1,91 @@
+package com.github.t1.metadeployer.lb.nginx;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.Response;
+import java.time.*;
+import java.util.*;
+
+import static java.time.Instant.*;
+import static java.util.stream.Collectors.*;
+import static javax.ws.rs.core.Response.Status.Family.*;
+
+public class LoadGenerator implements Runnable {
+    public static void main(String[] args) throws Exception {
+        LoadGenerator generator = new LoadGenerator();
+        generator.warmup();
+        Statistics statistics = new Statistics();
+        System.out.println("start");
+        Instant start = now();
+        for (int i = 0; i < 5000; i++) {
+            generator.run();
+            statistics.add(generator.time, generator.result);
+            // System.out.println(generator.time.toMillis() + ": " + generator.result);
+            if (i % 100 == 0)
+                System.out.print(".");
+            // Thread.sleep(3);
+        }
+        System.out.println("\ntotal: " + Duration.between(start, now()).toMillis());
+        System.out.println(statistics);
+    }
+
+    private final Client client = ClientBuilder.newClient();
+    private Duration time;
+    private String result;
+
+    private void warmup() throws Exception {
+        for (int i = 0; i < 10; i++)
+            run();
+    }
+
+    @Override public void run() {
+        WebTarget target = client.target("http://worker/jolokia/read/java.lang:type=Memory/HeapMemoryUsage");
+        Instant start = now();
+        this.result = fetch(target);
+        this.time = Duration.between(start, now());
+    }
+
+    private String fetch(WebTarget target) {
+        Response response = null;
+        try {
+            // response = completable(target.request().async().get()).get(10, MILLISECONDS);
+            response = target.request().get();
+            // System.out.println(response.getHeaderString("Content-Type"));
+            if (response.getStatusInfo().getFamily() == SUCCESSFUL)
+                return "success";
+            // System.out.println(response.readEntity(String.class));
+            return "failed " + response.getStatus() + " " + response.getStatusInfo();
+        } catch (Exception e) {
+            return "failed: " + e.getMessage();
+        } finally {
+            if (response != null)
+                response.close();
+        }
+    }
+
+    private static class Statistics {
+        private int count;
+        private long min = Integer.MAX_VALUE, avg = 0, max = 0;
+        private Map<String, Integer> results = new HashMap<>();
+
+        private void add(Duration time, String result) {
+            ++count;
+            long t = time.toMillis();
+            min = Math.min(min, t);
+            max = Math.max(max, t);
+            results.merge(result, 1, (left, right) -> left + right);
+        }
+
+        @Override public String toString() {
+            return ""
+                    + "count: " + count + "\n"
+                    + "min:   " + min + "\n"
+                    + "avg:   " + avg + "\n"
+                    + "max:   " + max + "\n"
+                    + "results:\n"
+                    + results.entrySet()
+                             .stream()
+                             .map(entry -> String.format("    %dx -> %s", entry.getValue(), entry.getKey()))
+                             .collect(joining("\n"));
+        }
+    }
+}
