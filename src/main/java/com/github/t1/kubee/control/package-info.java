0bf1cc4954e9f63a7d6c00bf2edcdf4c5f0e32e4
Rüdiger zu Dohna <snackbox@sinntr.eu>
2017-08-13 06:29:05 +0200

    updated versions; added jacoco

--- src/main/java/com/github/t1/kubee/control/package-info.java
+++ src/main/java/com/github/t1/kubee/control/package-info.java
@@ -2,6 +2,7 @@
         com.github.t1.kubee.gateway.deployer.DeployerGateway.class,
         com.github.t1.kubee.gateway.loadbalancer.LoadBalancerGateway.class,
         com.github.t1.kubee.model.Cluster.class,
+        com.github.t1.kubee.tools.http.ProblemDetail.class,
 })
 package com.github.t1.kubee.control;
 
