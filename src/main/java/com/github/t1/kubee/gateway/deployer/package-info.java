f6b270740e43984f38e95c55c4cf0ea7dc4e988a
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-03-25 09:28:22 +0100

    fix tests

--- src/main/java/com/github/t1/kubee/gateway/deployer/package-info.java
+++ src/main/java/com/github/t1/kubee/gateway/deployer/package-info.java
@@ -1,11 +1,9 @@
 @DependsUpon(packagesOf = {
-        com.github.t1.kubee.model.Deployment.class,
-        com.github.t1.kubee.tools.http.ProblemDetail.class,
+    com.github.t1.kubee.model.Deployment.class,
+    com.github.t1.kubee.tools.http.ProblemDetail.class,
 
-        com.fasterxml.jackson.core.JsonGenerator.class,
-        com.fasterxml.jackson.core.type.TypeReference.class,
-        com.fasterxml.jackson.databind.ObjectMapper.class,
-        com.fasterxml.jackson.dataformat.yaml.YAMLFactory.class,
-}) package com.github.t1.kubee.gateway.deployer;
+    com.fasterxml.jackson.core.type.TypeReference.class
+})
+package com.github.t1.kubee.gateway.deployer;
 
 import com.github.t1.testtools.DependsUpon;
---
+++ src/main/java/com/github/t1/kubee/tools/http/package-info.java
@@ -0,0 +1,9 @@
+@DependsUpon(packagesOf = {
+    com.fasterxml.jackson.core.JsonGenerator.class,
+    com.fasterxml.jackson.core.type.TypeReference.class,
+    com.fasterxml.jackson.databind.ObjectMapper.class,
+    com.fasterxml.jackson.dataformat.yaml.YAMLFactory.class,
+})
+package com.github.t1.kubee.tools.http;
+
+import com.github.t1.testtools.DependsUpon;
