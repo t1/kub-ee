5a20f2e427c606a069d4f84ac9bae19d70a2e9cc
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-11-06 16:42:50 +0100

    test docker in ansible

--- src/main/ansible/ee-worker/molecule/default/tests/test_ee-worker.py
+++ src/main/ansible/ee-worker/molecule/default/tests/test_ee-worker.py
@@ -4,9 +4,9 @@ import \
 import \
     testinfra.utils.ansible_runner
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+testinfra_hosts = testinfra.utils.ansible_runner.\
+    AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']).\
+    get_hosts('all')
 
 
 def test_hosts_file(host):
@@ -15,3 +15,15 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_is_docker_installed(host):
+    package_docker = host.package('docker-ce')
+
+    assert package_docker.is_installed
+
+
+def test_run_hello_world_container_successfully(host):
+    hello_world_ran = host.run("docker run hello-world")
+
+    assert 'Hello from Docker!' in hello_world_ran.stdout
