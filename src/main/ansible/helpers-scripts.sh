5cac41aa4a416992ce4e41eddd800bdc25e0d2aa
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-11-11 17:18:43 +0100

    ansible with vagrant

--- src/main/ansible/helpers-scripts.sh
+++ src/main/ansible/helpers-scripts.sh
@@ -3,3 +3,8 @@ sudo -u wildfly /opt/wildfly/bin/jboss-cli.sh --connect
 
 sudo -u wildfly tail -f /opt/wildfly/standalone/log/server.log
 
+curl http://localhost:8080/deployer -H'Accept: application/yaml'
+
+curl -d "jolokia.version: 1.3.2" -X POST http://localhost:8080/deployer -H'Content-Type: application/yaml' -H'Accept: application/yaml'
+
+sudo touch /opt/wildfly/standalone/deployments/deployer.war
