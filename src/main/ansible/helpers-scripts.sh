a3976ddb5e4855f323d61abb76f3e0dc6a2aa59d
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-11-14 14:07:14 +0100

    more helper scripts

--- src/main/ansible/helpers-scripts.sh
+++ src/main/ansible/helpers-scripts.sh
@@ -3,8 +3,10 @@ sudo -u wildfly /opt/wildfly/bin/jboss-cli.sh --connect
 
 sudo -u wildfly tail -f /opt/wildfly/standalone/log/*.log
 
-curl http://localhost:8080/deployer -H'Accept: application/yaml'
+sudo touch /opt/wildfly/standalone/deployments/deployer.war
+
+cat /opt/wildfly/standalone/configuration/deployer.root.bundle
+
+curl http://localhost:8080/jolokia
 
 curl -d "jolokia.version: 1.3.2" -X POST http://localhost:8080/deployer -H'Content-Type: application/yaml' -H'Accept: application/yaml'
-
-sudo touch /opt/wildfly/standalone/deployments/deployer.war
